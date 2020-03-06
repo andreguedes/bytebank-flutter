5947fdbc6e785eebbe1a8d9e6a42019aa3bda3b8
andreguedes <andresguedes@live.com>
2020-03-06 01:08:56 -0300

    Add http request

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
+import 'package:bytebank/http/webclient.dart';
 import 'package:bytebank/screens/dashboard.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(ByteBankApp());
+  findAll();
 }
 
 class ByteBankApp extends StatelessWidget {
