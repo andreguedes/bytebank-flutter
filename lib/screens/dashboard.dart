8093899caf4a6cce29ffdc2c9f5e5c2046a08fb0
andreguedes <andresguedes@live.com>
2020-03-06 00:40:38 -0300

    Change dashboard items to listview and show transactions list screen

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -1,4 +1,5 @@
 import 'package:bytebank/screens/contacts_list.dart';
+import 'package:bytebank/screens/transactions_list.dart';
 import 'package:flutter/material.dart';
 
 class Dashboard extends StatelessWidget {
@@ -8,39 +9,71 @@ class Dashboard extends StatelessWidget {
       appBar: AppBar(
         title: Text('Dashboard'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Image.asset('images/bytebank_logo.png'),
-            Material(
-              color: Theme.of(context).primaryColor,
-              child: InkWell(
-                onTap: () {
-                  Navigator.of(context).push(
-                      MaterialPageRoute(builder: (context) => ContactsList()));
-                },
-                child: Container(
-                  height: 100,
-                  width: 150,
-                  padding: EdgeInsets.all(8.0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Icon(Icons.people, color: Colors.white, size: 24.0),
-                      Text(
-                        'Contacts',
-                        style: TextStyle(color: Colors.white, fontSize: 16.0),
-                      )
-                    ],
-                  ),
-                ),
-              ),
-            )
-          ],
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Image.asset('images/bytebank_logo.png')),
+          Container(
+            height: 120,
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: <Widget>[
+                _FeatureItem('Transfer', Icons.monetization_on,
+                    onClick: () => _showContactList(context)),
+                _FeatureItem('Transaction feed', Icons.description,
+                    onClick: () => _showTransactionsList(context)),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _showContactList(BuildContext context) {
+    Navigator.of(context)
+        .push(MaterialPageRoute(builder: (context) => ContactsList()));
+  }
+
+  void _showTransactionsList(BuildContext context) {
+    Navigator.of(context)
+        .push(MaterialPageRoute(builder: (context) => TransactionsList()));
+  }
+}
+
+class _FeatureItem extends StatelessWidget {
+  final String name;
+  final IconData icon;
+  final Function onClick;
+
+  _FeatureItem(this.name, this.icon, {@required this.onClick});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(8.0),
+      child: Material(
+        color: Theme.of(context).primaryColor,
+        child: InkWell(
+          onTap: () => onClick(),
+          child: Container(
+            width: 150,
+            padding: EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Icon(icon, color: Colors.white, size: 24.0),
+                Text(
+                  name,
+                  style: TextStyle(color: Colors.white, fontSize: 16.0),
+                )
+              ],
+            ),
+          ),
         ),
       ),
     );
