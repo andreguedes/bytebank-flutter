3422427a22886734c3b44fbbddb6e79b45ecb4e7
andreguedes <andresguedes@live.com>
2020-03-06 17:50:14 -0300

    Save transfer

--- lib/http/webclient.dart
+++ lib/http/webclient.dart
@@ -5,12 +5,14 @@ import 'package:bytebank/models/transaction.dart';
 import 'package:http/http.dart';
 import 'package:http_interceptor/http_interceptor.dart';
 
+final Client client =
+    HttpClientWithInterceptor.build(interceptors: [LoggingInterceptor()]);
+
+const String BASE_URL = 'http://192.168.10.73:8080/transactions';
+
 Future<List<Transaction>> findAll() async {
-  final Client client =
-      HttpClientWithInterceptor.build(interceptors: [LoggingInterceptor()]);
-  final Response response = await client
-      .get('http://192.168.10.73:8080/transactions')
-      .timeout(Duration(seconds: 5));
+  final Response response =
+      await client.get(BASE_URL).timeout(Duration(seconds: 5));
   final List<dynamic> decodeJson = jsonDecode(response.body);
   final List<Transaction> transactions = List();
   for (Map<String, dynamic> transactionJson in decodeJson) {
@@ -20,6 +22,27 @@ Future<List<Transaction>> findAll() async {
     transactions.add(transaction);
   }
   return transactions;
+}
+
+Future<Transaction> save(Transaction transaction) async {
+  final Map<String, dynamic> transactionMap = {
+    'value': transaction.value,
+    'contact': {
+      'name': transaction.contact.name,
+      'accountNumber': transaction.contact.accountNumber,
+    }
+  };
+  final Response response = await client.post(
+    BASE_URL,
+    headers: {'Content-Type': 'application/json', 'password': '1000'},
+    body: jsonEncode(transactionMap),
+  );
+  final Map<String, dynamic> decodeJson = jsonDecode(response.body);
+  final Map<String, dynamic> contactJson = decodeJson['contact'];
+  return Transaction(
+    decodeJson['value'],
+    Contact(0, contactJson['name'], contactJson['accountNumber']),
+  );
 }
 
 class LoggingInterceptor implements InterceptorContract {
